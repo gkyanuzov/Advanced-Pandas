ab1ca5f1dda62cf4123d821c3ff61eda7dedb6cd
DESKTOP-GVP3BTH\USER <gkyanuzov@gmail.com>
2023-07-13 18:34:52 +0300

    finished the first exercise

--- Ch1 - From Beginner to Advanced pandas/Intro to DataFrames using pandas.py
+++ Ch1 - From Beginner to Advanced pandas/Intro to DataFrames using pandas.py
@@ -1,1 +1,20 @@
-ss = []
+import pandas as pd
+
+scores = {"name":['Ray','Japhy','Zosa'],
+          "city":['San Francisco','San Francisco','Denver'],
+          "score":[75,92,94]
+         }
+
+
+df = pd.DataFrame(scores)
+print(df)
+
+print(df['score'])
+print(df.score)
+
+#create new col
+df['name_city'] = df['name'] + '__' + df['city']
+print(df)
+
+over_90 = df[df['score'] > 90]
+print(over_90)
